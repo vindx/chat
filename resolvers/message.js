91c4d77efe729c12aaeb0baa75e5654629d7e7d7
vindx <kiruha9414@gmail.com>
2020-04-15 15:17:25 +0300

    created some functionality for messages

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,1 +1,27 @@
-module.exports = {};
+module.exports = {
+  Query: {
+    getMessage: async (parent, { id }, { models }) => {
+      try {
+        const message = await models.Message.findById(id);
+        if (!message) {
+          throw new Error("Message didn't found");
+        }
+        return message;
+      } catch (err) {
+        return err;
+      }
+    },
+    getAllMessages: async (parent, args, { models }) => await models.Message.find()
+  },
+  Mutation: {
+    createMessage: async (parent, args, { models }) => {
+      try {
+        const newMessage = new models.Message(args);
+        await newMessage.save();
+        return newMessage;
+      } catch (err) {
+        return err;
+      }
+    }
+  }
+};
--- schemas/message.js
+++ schemas/message.js
@@ -1,7 +1,19 @@
 module.exports = `
-type Message {
-  id: ID!
-  text: String!
-  user: User!
-  channel: Channel!
-}`;
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
+    channel: Channel!
+  }
+  
+   type Query {
+    getMessage(id: ID!): Message!
+    getAllMessages: [Message!]!
+  }
+  
+  type Mutation {
+    createMessage(channelId: ID!, text: String!): Message!
+  }
+  
+  
+`;
