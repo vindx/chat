ae588d022a99dda487169a1b73160b27b3343c5d
vindx <kiruha9414@gmail.com>
2020-04-15 15:04:10 +0300

    created some functionality for channels

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -1,1 +1,27 @@
-module.exports = {};
+module.exports = {
+  Query: {
+    getChannel: async (parent, { id }, { models }) => {
+      try {
+        const channel = await models.Channel.findById(id);
+        if (!channel) {
+          throw new Error("Channel didn't found");
+        }
+        return channel;
+      } catch (err) {
+        return err;
+      }
+    },
+    getAllChannels: async (parent, args, { models }) => await models.Channel.find()
+  },
+  Mutation: {
+    createChannel: async (parent, args, { models }) => {
+      try {
+        const newChannel = new models.Channel(args);
+        await newChannel.save();
+        return newChannel;
+      } catch (err) {
+        return err;
+      }
+    }
+  }
+};
--- schemas/channel.js
+++ schemas/channel.js
@@ -1,8 +1,18 @@
 module.exports = `
-type Channel {
-  id: ID!
-  name: String!
-  owner: User!
-  messages: [Message!]
-  members: [User!]!
-}`;
+  type Channel {
+    id: ID!
+    name: String!
+    owner: User!
+    messages: [Message!]
+    members: [User!]!
+  }
+  
+  type Query {
+    getChannel(id: ID!): Channel!
+    getAllChannels: [Channel!]!
+  }
+  
+  type Mutation {
+    createChannel(name: String!): Channel!
+  }
+`;
