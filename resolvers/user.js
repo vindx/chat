e81287f2d30ac504cce88ff74c4a77bf06290302
vindx <kiruha9414@gmail.com>
2020-04-15 13:48:50 +0300

    try, catch (added catch err "user didn't found)"

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,27 @@
 module.exports = {
   Query: {
-    getUser: async (parent, { id }, { models }) => await models.User.findById(id),
+    getUser: async (parent, { id }, { models }) => {
+      try {
+        const user = await models.User.findById(id);
+        if (!user) {
+          throw new Error("User didn't found");
+        }
+        return user;
+      } catch (err) {
+        return err;
+      }
+    },
     getAllUsers: async (parent, args, { models }) => await models.User.find()
   },
   Mutation: {
-    createUser: async (parent, args, { models }) => await new models.User(args).save()
+    createUser: async (parent, args, { models }) => {
+      try {
+        const newUser = new models.User(args);
+        await newUser.save();
+        return newUser;
+      } catch (err) {
+        return err;
+      }
+    }
   }
 };
