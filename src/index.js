8d7f934ceba2c9a99d479b2e9962c9c79b027b27
vindx <kiruha9414@gmail.com>
2020-04-16 13:36:12 +0300

    installed semantic-ui-react library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
+import 'semantic-ui-css/semantic.min.css';
 
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
