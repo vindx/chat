ca293bf0f5c4f1171f37224b7e9bc2c74a61e31b
vindx <kiruha9414@gmail.com>
2020-04-17 13:32:41 +0300

    created simple createChannel page

---
+++ src/routes/CreateChannel.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Form, Button, Container, Header, Input, Message } from 'semantic-ui-react';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
+import PropTypes from 'prop-types';
+
+const createChannelMutation = gql`
+  mutation($name: String!) {
+    createChannel(name: $name) {
+      ok
+      errors {
+        path
+        type
+        message
+      }
+    }
+  }
+`;
+
+const CreateChannel = (props) => {
+  const [createChannel, { loading }] = useMutation(createChannelMutation);
+  const [data, setData] = useState({
+    channelName: '',
+    channelNameError: '',
+  });
+  const { channelName, channelNameError } = data;
+
+  const handleChangeData = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value, [`${name}Error`]: null });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await createChannel({ variables: { name: channelName } });
+      const { ok, errors } = response.data.createChannel;
+
+      if (ok) {
+        props.history.push('/');
+      } else {
+        const err = errors.reduce((acc, { path, message }) => {
+          acc[`${path}Error`] = message;
+          return acc;
+        }, {});
+        setData({ ...data, ...err });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container text>
+      <Header as="h2">Create a channel</Header>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field error={!!channelNameError}>
+          <Input
+            fluid
+            placeholder="Enter channel name"
+            name="channelName"
+            value={channelName}
+            onChange={handleChangeData}
+          />
+        </Form.Field>
+        <Button negative={!!channelNameError} loading={loading} compact>
+          Submit
+        </Button>
+      </Form>
+      {channelNameError && (
+        <Message
+          error
+          header="There was some errors with your submission"
+          list={[channelNameError]}
+        />
+      )}
+    </Container>
+  );
+};
+
+CreateChannel.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default CreateChannel;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
+import CreateChannel from './CreateChannel';
 
 export default () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/createChannel" exact component={CreateChannel} />
     </Switch>
   </BrowserRouter>
 );
