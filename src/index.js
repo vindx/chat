136d368dc7e09f6a2ae00962e935bc1af022ad10
vindx <kiruha9414@gmail.com>
2020-04-17 13:39:59 +0300

    added tokens to qgl context

--- src/index.js
+++ src/index.js
@@ -3,20 +3,33 @@ import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import { ApolloProvider } from '@apollo/react-hooks';
 import 'semantic-ui-css/semantic.min.css';
 
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
+const httpLink = new HttpLink({
   uri: 'http://localhost:5000/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  return {
+    headers: {
+      ...headers,
+      'auth-token': token,
+      'auth-refresh-token': refreshToken,
+    },
+  };
+});
+
 const client = new ApolloClient({
-  cache,
-  link,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 const App = (
