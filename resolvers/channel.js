a01d4b403ccffe9c9b1a1be2733eaee6c84e2988
vindx <kiruha9414@gmail.com>
2020-04-20 14:25:11 +0300

    correct channel's queries

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -14,17 +14,29 @@ module.exports = {
         return err;
       }
     },
-    getAllChannels: async (parent, args, { models }) => await models.Channel.find(),
+    getAllChannels: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => await models.Channel.find({ owner: user.id })
+    ),
   },
   Mutation: {
     createChannel: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const newChannel = new models.Channel({ ...args, owner: user.id });
+        const newChannel = new models.Channel({ ...args, owner: user.id, members: [user.id] });
         await newChannel.save();
         return { ok: true };
       } catch (err) {
         return { ok: false, errors: formatErrors(err) };
       }
     }),
+  },
+  Channel: {
+    owner: async ({ owner }, args, { models }) => await models.User.findById(owner),
+    messages: async ({ id }, args, { models }) => await models.Message.find({ channelId: id }),
+    members: async ({ id }, args, { models }) => {
+      const { members: membersIds } = await models.Channel.findById(id);
+      return await Promise.all(
+        membersIds.map(async (memberId) => await models.User.findById(memberId))
+      );
+    },
   },
 };
