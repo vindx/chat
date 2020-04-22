1797cb44b5f07e23dd6120b9198309d49cf244fb
vindx <kiruha9414@gmail.com>
2020-04-22 18:10:13 +0300

    graphQL subscriptions setup

---
+++ src/apollo.js
@@ -0,0 +1,73 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink, from, split } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:5000/graphql',
+});
+
+// Setup the header for the request
+const middlewareAuthLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  return {
+    headers: {
+      ...headers,
+      'auth-token': token,
+      'auth-refresh-token': refreshToken,
+    },
+  };
+});
+
+// After the backend responds, we take the refreshToken from headers if it exists
+// and save it in the localstorage
+
+// eslint-disable-next-line arrow-body-style
+const afterWareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    const {
+      response: { headers },
+    } = operation.getContext();
+
+    if (headers) {
+      const token = headers.get('auth-token');
+      const refreshToken = headers.get('auth-refresh-token');
+      if (token && refreshToken) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('refreshToken', refreshToken);
+      }
+    }
+
+    return response;
+  });
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5000/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  from([middlewareAuthLink, afterWareLink, httpLink])
+);
+
+export default new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
--- src/index.js
+++ src/index.js
@@ -1,61 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink, from } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+
 import { ApolloProvider } from '@apollo/react-hooks';
 import 'semantic-ui-css/semantic.min.css';
 
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
-
-const httpLink = new HttpLink({
-  uri: 'http://localhost:5000/graphql',
-});
-
-// Setup the header for the request
-const middlewareAuthLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
-  const refreshToken = localStorage.getItem('refreshToken');
-
-  return {
-    headers: {
-      ...headers,
-      'auth-token': token,
-      'auth-refresh-token': refreshToken,
-    },
-  };
-});
-
-// After the backend responds, we take the refreshToken from headers if it exists
-// and save it in the localstorage
-
-// eslint-disable-next-line arrow-body-style
-const afterWareLink = new ApolloLink((operation, forward) => {
-  return forward(operation).map((response) => {
-    const {
-      response: { headers },
-    } = operation.getContext();
-
-    if (headers) {
-      const token = headers.get('auth-token');
-      const refreshToken = headers.get('auth-refresh-token');
-      if (token && refreshToken) {
-        localStorage.setItem('token', token);
-        localStorage.setItem('refreshToken', refreshToken);
-      }
-    }
-
-    return response;
-  });
-});
-
-const client = new ApolloClient({
-  link: from([middlewareAuthLink, afterWareLink, httpLink]),
-  cache: new InMemoryCache(),
-});
+import client from './apollo';
 
 const App = (
   <ApolloProvider client={client}>
