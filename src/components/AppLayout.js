8b4f52e8c396b8f596ac057806c0a425d85a3b76
vindx <kiruha9414@gmail.com>
2020-04-22 17:11:18 +0300

    changed messages layout a bit

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export default styled.div`
   display: grid;
-  min-height: 100vh;
+  height: 100vh;
   grid-template-columns: 75px 200px 1fr;
   grid-template-rows: auto 1fr auto;
 `;
--- src/containers/MessagesContainer.js
+++ src/containers/MessagesContainer.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+import { Comment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 import Messages from '../components/Messages';
 
 const getMessagesQuery = gql`
-  query($id: ID!) {
-    getChannel(id: $id) {
-      messages {
-        id
-        text
+  query($channelId: ID!) {
+    getMessages(channelId: $channelId) {
+      id
+      text
+      user {
+        userName
       }
+      createdAt
     }
   }
 `;
 
 const MessagesContainer = ({ currentChannelId = '' }) => {
-  const { loading, error, data: { getChannel: { messages } = {} } = {} } = useQuery(
-    getMessagesQuery,
-    {
-      variables: { id: currentChannelId },
-    }
-  );
+  const { loading, error, data: { getMessages: messages = {} } = {} } = useQuery(getMessagesQuery, {
+    variables: { channelId: currentChannelId },
+  });
 
   if (loading) return <p>Loading...</p>;
-  if (!messages) {
+  if (!messages.length) {
     return (
       <Messages style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         Please choose channel
@@ -36,11 +36,19 @@ const MessagesContainer = ({ currentChannelId = '' }) => {
 
   return (
     <Messages>
-      <ul>
-        {messages.map(({ id, text }) => (
-          <li key={`message-${id}`}>{text}</li>
+      <Comment.Group>
+        {messages.map(({ id, text, user: { userName }, createdAt }) => (
+          <Comment key={`message-${id}`}>
+            <Comment.Content>
+              <Comment.Author as="a">{userName}</Comment.Author>
+              <Comment.Metadata>
+                <div>{new Date(createdAt * 1000).toString()}</div>
+              </Comment.Metadata>
+              <Comment.Text>{text}</Comment.Text>
+            </Comment.Content>
+          </Comment>
         ))}
-      </ul>
+      </Comment.Group>
     </Messages>
   );
 };
