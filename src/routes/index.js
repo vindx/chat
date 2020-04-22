dd896513787b5482bf37631419eb12d99166e787
vindx <kiruha9414@gmail.com>
2020-04-22 13:20:33 +0300

    delete CreateChannel page cause CreateChannel functionality replaced to view-channel page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
-import CreateChannel from './CreateChannel';
 import ViewChannel from './ViewChannel';
 
 const isAuthenticated = () => {
@@ -36,7 +35,6 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
-      <PrivateRoute path="/create-channel" exact component={CreateChannel} />
       <PrivateRoute path="/view-channel/:channelId?" exact component={ViewChannel} />
     </Switch>
   </BrowserRouter>
