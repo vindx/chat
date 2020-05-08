95376772accf2306c3793dcdc69ecdb49b78130b
vindx <kiruha9414@gmail.com>
2020-05-08 13:10:22 +0300

    updated CreateChannel func with handling some errors

--- models/Channel.js
+++ models/Channel.js
@@ -1,7 +1,12 @@
 const { Schema, model, Types } = require('mongoose');
 
 const channelSchema = new Schema({
-  name: { type: String, required: [true, 'Name is required'] },
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'The name needs to be at least 3 characters long'],
+    maxlength: [20, 'The name needs to be not more 20 characters long'],
+  },
   owner: { type: Types.ObjectId, ref: 'User' },
   members: [{ type: Types.ObjectId, ref: 'User' }],
   messages: [{ type: Types.ObjectId, ref: 'Message' }],
