51cb15df5f90e197e5453ca47335b9561813f320
vindx <kiruha9414@gmail.com>
2020-05-13 12:48:35 +0300

    changed position of elements at InvitePeopleModal (general style)

--- src/components/InvitePeopleModal.js
+++ src/components/InvitePeopleModal.js
@@ -37,58 +37,48 @@ const InvitePeopleModal = ({ isOpen, onClose, channelId }) => {
       <Modal.Content>
         <Segment>
           <Grid columns={2} relaxed="very">
-            <Grid.Row centered>
-              <Grid.Column verticalAlign="middle">
-                <Header as="h2" textAlign="center">
-                  Create a secret
-                  <Label color="green" horizontal>
-                    KEY
-                  </Label>
-                  !
-                </Header>
-              </Grid.Column>
-              <Grid.Column>
-                <Header as="h5" disabled textAlign="center">
-                  Click on generate button
-                </Header>
-                <Button
-                  basic
-                  positive
-                  fluid
-                  onClick={handleGenerateChannelKey}
-                  loading={loading}
-                  disabled={loading}
-                >
-                  Generate
-                </Button>
-              </Grid.Column>
-            </Grid.Row>
+            <Grid.Column>
+              <Header as="h3" textAlign="center">
+                Create a secret
+                <Label color="green" horizontal>
+                  KEY
+                </Label>
+                !
+              </Header>
+              <Header as="h5" disabled textAlign="center">
+                Click on generate button
+              </Header>
+              <Button
+                basic
+                positive
+                fluid
+                onClick={handleGenerateChannelKey}
+                loading={loading}
+                disabled={loading}
+              >
+                Generate
+              </Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Header as="h3" textAlign="center">
+                Share a
+                <Label color="green" horizontal>
+                  KEY
+                </Label>
+                !
+              </Header>
+              <Header as="h5" disabled textAlign="center">
+                Share this key to your friends
+              </Header>
+              <Header as="h5" textAlign="center">
+                <Label color="green" horizontal>
+                  key
+                </Label>
+                {shortId}
+              </Header>
+            </Grid.Column>
           </Grid>
-          <Divider horizontal>and</Divider>
-          <Grid columns={2} relaxed="very">
-            <Grid.Row centered>
-              <Grid.Column verticalAlign="middle">
-                <Header as="h2" textAlign="center">
-                  Share a
-                  <Label color="green" horizontal>
-                    KEY
-                  </Label>
-                  !
-                </Header>
-              </Grid.Column>
-              <Grid.Column>
-                <Header as="h5" disabled textAlign="center">
-                  Share this key to your friends
-                </Header>
-                <Header as="h5" textAlign="center">
-                  <Label color="green" horizontal>
-                    key
-                  </Label>
-                  {shortId}
-                </Header>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <Divider vertical>and</Divider>
         </Segment>
       </Modal.Content>
     </Modal>
